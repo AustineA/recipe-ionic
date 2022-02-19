d4c429fb93671c4a7fd7f248a9e6b8f99bfc6128
Austine Amah <austine.amah@gmail.com>
2022-02-19 15:12:59 +0100

    home page loop

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -6,7 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  recipes = [
+    {
+      recipe: 'fish',
+      featuredImage: '../../assets/images/Fish.jpg',
+    },
+    {
+      recipe: 'chicken',
+      featuredImage: '../../assets/images/Chicken.jpg',
+    },
+    {
+      recipe: 'beef',
+      featuredImage: '../../assets/images/Beef.jpg',
+    },
+    {
+      recipe: 'juice',
+      featuredImage: '../../assets/images/Fruit.jpg',
+    },
+    {
+      recipe: 'vega',
+      featuredImage: '../../assets/images/Vegan.jpg',
+    },
+    {
+      recipe: 'salad',
+      featuredImage: '../../assets/images/Salad.jpg',
+    },
+  ];
 
   constructor() {}
 
+  goTo(recipe) {
+    console.log(recipe);
+  }
+  search() {}
 }
